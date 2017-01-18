4f6f6fc24bfa99e96a2acd5158331533091bc6c7
alexisherrera <halexis@sas.upenn.edu>
2017-01-18 12:46:03 -0500

    Added Sine Wavesgit add *

--- src/serenade/Key.java
+++ src/serenade/Key.java
@@ -2,12 +2,15 @@ package serenade;
 
 import java.awt.Color;
 
+//represents a key in the keyboard
 public class Key {
     private BufferingQueue buffer; 
     static int SAMP_RATE = 44100; 
     static double ENERGY_DECAY_FACTOR = -0.997; 
     private int numSamples; 
     private int counter = 0;
+    private int counterWaves = 0;
+    private double waveX = 0;
     public Key(double frequency) {
         this.numSamples = (int) Math.ceil(SAMP_RATE / frequency);
         this.buffer = new BufferingQueue(numSamples);
@@ -29,15 +32,15 @@ public class Key {
     
     //advance the simulation one time step using Karplus-strong calculation
     public void tic() {
-        // pause for 20 ms
-    	
         double firstSamp = buffer.dequeue();
         double secondSamp = buffer.peek();
         double addSamp = firstSamp + secondSamp;
         double avgSamp = addSamp / 2.0;
         double avgKarpStrong = avgSamp * ENERGY_DECAY_FACTOR;
         buffer.enqueue(avgKarpStrong);
+        //chaos mode for the visualizations
         if (StdDraw.chaosMode) {
+        	counterWaves = 0;
         	if (buffer.peek() > 0.01) {
             	if (counter == 200) {
                 	StdDraw.line(Math.random(), Math.random(), Math.random(), Math.random());
@@ -46,12 +49,24 @@ public class Key {
             	 counter++;
             }
         }
+        //wave mode for visualization
         if (StdDraw.wavesMode) {
-        	if (counter == 200) {
-        		
+        	counter = 0;
+        	if (buffer.peek() > 0.01) {
+        		if (counterWaves == 200) {
+	        		//System.out.println("hello");
+	    			if (this.waveX >= 1) { this.waveX = 0; }
+	    			double xV = this.waveX;
+	    			double yV = avgKarpStrong * Math.sin(10 * xV) + .6;
+	    			this.waveX = this.waveX + .01;
+	    			double xVV = this.waveX;
+	    			double yVV = avgKarpStrong * Math.sin(10 * xVV) + .6;
+	    			StdDraw.line(xV, yV, xVV, yVV);
+	    			counterWaves = 0;
+        		}
+        		counterWaves++;
         	}
         }
-        
     }
     
     // return the current sample
